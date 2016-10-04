f728c800791a582fdd15db21b9abdc1ee5679357
Steven Anderson <stevenanderson@Stevens-MacBook-Pro.local>
2016-10-04 17:07:57 -0600

    answers as they were after the 3 hours were up

--- src/assessment.js
+++ src/assessment.js
@@ -10,10 +10,10 @@
 
 //         function son(){
 //             var playnintendo = "Mario";
-//         }   
+//         }
 //     }
 
-//     function uncle(){        
+//     function uncle(){
 //         var badjoke = "Har";
 
 //         function cousin(){
@@ -43,13 +43,33 @@ var scopeArray5 = ["grandpa", "dad", "son", "uncle", "cousin"];
 
 // #2  ###################
 // # Promises
-var foo;
+
 /* Write a function called async.
   Use $q to create a promise object and return the promise.
   Call setTimeout on a function
   which changes the variable foo (above) to 'bar'
   And resolve the promise when setTimeout completes.
 */
+var foo;
+
+// var defered = $q.defer();
+//
+//             setTimeout(function() {
+//                 phrase = 'good by suckers';
+//                 defered.resolve(phrase);
+//             }, 3000);
+//
+//             return defered.promise;
+
+// this.async = function('foo') {
+//   var deferred = $q.defer();
+//   setTimeout(function() {
+//     foo = 'bar';
+//     deferred.resolve(foo);
+//   }, 500);
+//
+//   return deferred.promise;
+// }
 
 
 // #3  ###################
@@ -57,6 +77,9 @@ var foo;
 // Write a function called context1 that takes in 4 parameters: A function called myFn, an object called context, param1, and param2.
 // Invoke myFn explicitly setting the context to the object called context.  Pass in param1 and param2 in order as well.
 
+function context1(callback, obj, param1, param2) {
+  callback(param1, param2).call(obj);
+}
 
 
 
@@ -64,6 +87,11 @@ var foo;
 // # Context 2
 // Write a function called context2 that takes in 3 parameters: A function called myFn, an object called context, and an array called params
 // Invoke myFn explicitly setting the context to the object called context.  Pass in params
+function context2(callback, obj, arr) {
+  arr.apply(obj);
+  callback(this.arr);
+}
+
 
 
 
@@ -72,27 +100,45 @@ var foo;
 // # Context 3
 // Write a function called context3 that takes in 2 parameters: A function called myFn, and an object called context
 // Make sure the function is permanently linked to the context.  This should give you a new function, return it.
-
+function context3(callback, obj) {
+  return callback(obj.bind);
+}
 
 
 
 // #6  ###################
 // # Constructor Function
 // Make a constructor function called taco that takes in 3 parameters: shell, meat, veggies and assigns them to identically named properties.
-
+function Taco(shell, meat, veggies) {
+  this.shell = shell;
+  this.meat = meat;
+  this.veggies = veggies;
+}
 
 
 
 // #7  ###################
 // # Implicit binding
 // Make a constructor function called burrito.  It has a property called percentLeft = 100.  It has a property called eat that is a function.  When eat is invoked it uses context to implicitly subract 25 from the percentLeft on the burrito.
-
+function Burrito() {
+  this.percentLeft = 100;
+  this.eat = function() {
+    return Burrito.percentLeft -= 25;
+  };
+}
 
 
 // #8  ###################
-// # Prototype 1 
+// # Prototype 1
 // Add prototype function to the array type that doubles the value of every item in the array
-
+Array.prototype.doubler = function() {
+  var arr = this;
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] += arr[i];
+  }
+  return arr;
+}
 
 
 
@@ -100,22 +146,33 @@ var foo;
 // # Prototype 2
 // Write a constructor function called chimichanga.  It has a property called percentLeft = 100.  It has a prototype function called eat.  When eat is invoked it uses context to implicitly subract 20 from the precentLeft on the chimichanga.
 
-
+function Chimichanga() {
+  this.percentLeft = 100;
+  eat = function() {
+    percentLeft -= 20;
+  };
+}
+// create a variable chimichanga inside of the function.
 
 
 // #10  ###################
 // # Closure 1
 // Write a function called sentence machine.  It takes in a parameter called partOne.  It returns a function called sentenceSmasher.
-// When sentenceSmasher is invoked it should take in a parameter called partTwo and return a new string that adds partOne and partTwo together.  
+// When sentenceSmasher is invoked it should take in a parameter called partTwo and return a new string that adds partOne and partTwo together.
+function sentenceMachine(param1, callback) {
+  return callback();
+}
 
-
+function sentenceSmasher(param1, param2, callback) {
+  return callback(param1 + param2);
+}
 
 // #11  ###################
 // # Closure 2
-// Write a function called subway.  It takes in one parameter called personName.  
+// Write a function called subway.  It takes in one parameter called personName.
 // It needs to keep track of what ingredients the person wants on their sandwhich.
-// It needs to do this by returning a function called addIngredient.  
-// When addIngredient is invoked it saves that ingredient with previously added ingredients and then returns an object that looks like this (Replace the parts wrapped in <> with correct data): 
+// It needs to do this by returning a function called addIngredient.
+// When addIngredient is invoked it saves that ingredient with previously added ingredients and then returns an object that looks like this (Replace the parts wrapped in <> with correct data):
 // ```
 // {
 //     orderPerson: <Person name goes here>,
@@ -130,4 +187,14 @@ var foo;
 // Write a function that takes in 2 parameters.
 // If both parameters are the same type an the same value return "Exact match".
 // If both parameters have the same value but are different types return "Different types"
-// Otherwise return "Different values" 
+// Otherwise return "Different values"
+
+function compareValues(param1, param2) {
+  if (typeof param1 === typeof param2) {
+    return "Exact match";
+  } else if (param1 == param2) {
+    return "Different types";
+  } else {
+    return "Different values";
+  }
+}
